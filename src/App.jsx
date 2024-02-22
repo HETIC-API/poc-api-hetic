0a135eaf484ad28e6a1362ec497f2cce5f54e305
Lucas YALMAN <yalmanlucas@gmail.com>
2024-02-22 15:36:18 +0100

    front init

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import './App.css'
+import './App.css';
+import { createRoot } from 'react-dom/client';
+import Timer from './components/Timer/Timer';
+import Scene from './components/Scene/Scene';
+import { Canvas } from '@react-three/fiber';
 
-function App() {
+export default function App() {
 
   return (
     <>
-
+        <Timer />
+        <Canvas className='canvas-container' camera={{ fov: 40, position: [0, 0, 10]}}>
+            <Scene />
+        </Canvas>
     </>
   )
 }
 
-export default App
+createRoot(document.getElementById('root')).render(<App />)
